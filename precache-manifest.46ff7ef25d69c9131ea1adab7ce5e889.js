fff2ad5d50f35b405a762fb10a72138d1629a7fd
Ross Gartland <rgartland117@hotmail.com>
2020-04-09 02:55:06 +0100

    Updates

---
+++ precache-manifest.46ff7ef25d69c9131ea1adab7ce5e889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b69b291e483c22141354916d3fd92e",
+    "url": "/covid-ninteen-ni/index.html"
+  },
+  {
+    "revision": "40f820ec76c27860af1f",
+    "url": "/covid-ninteen-ni/static/css/main.10975dd1.chunk.css"
+  },
+  {
+    "revision": "a74610ea77723fce7da5",
+    "url": "/covid-ninteen-ni/static/js/2.4f44d225.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-ninteen-ni/static/js/2.4f44d225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40f820ec76c27860af1f",
+    "url": "/covid-ninteen-ni/static/js/main.4261cec5.chunk.js"
+  },
+  {
+    "revision": "d9cd48cc1fa092699b21",
+    "url": "/covid-ninteen-ni/static/js/runtime-main.de4a6f3a.js"
+  }
+]);
