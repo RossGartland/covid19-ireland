c76945a3d578be603b2f74407c6fb5c3d4981766
Ross Gartland <rgartland117@hotmail.com>
2020-04-14 13:59:10 +0100

    Navbar rebuilt to use React bootstrap nav

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 import './nav.style.css';
 
-const Nav = () => {
-    
+const MyNav = () => {
+
     const [scrollPosition, setSrollPosition] = useState(0);
     const handleScroll = () => {
         const position = window.pageYOffset;
@@ -13,39 +13,27 @@ const Nav = () => {
 
     window.addEventListener('scroll', handleScroll, { passive: true });
 
-    return (
-        <div>
-            <nav class="navbar navbar-expand-lg navbar-light navbar-inverse nav-fill w-100" style={{ background: window.scrollY > 0 ? "white" : "transparent" }}>
-                <a class="navbar-brand" href="#">COVID-19 IRELAND</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li>
-                            <a class="nav-link active" href="#">HOME <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li >
-                            <a class="nav-link active" href="#/map">MAP</a>
-                        </li>
-                        <li class="nav-item dropdown active">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                INFO</a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            
-                                <a class="dropdown-item" href="#/covidupdates">Updates</a>
-                                <a class="dropdown-item" href="#/work">WORK</a>
-                                <a class="dropdown-item" href="#/faq">F&Q</a>
-                            </div>
-                        </li>
-                        <li>
-                            <a class="nav-link active" href="#/about">ABOUT</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+    return (
+        <div>     
+            <Navbar collapseOnSelect className="navbar navbar-expand-lg navbar-light navbar-inverse nav-fill w-100" expand="lg" style={{ background: window.scrollY > 0 ? "white" : "transparent" }}>
+                <Navbar.Brand href="#">COVID-19 IRELAND</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" >
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#/">Home</Nav.Link>
+                        <Nav.Link href="#/map">Map</Nav.Link>
+                        <NavDropdown title="Info" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#/covidupdates">Updates</NavDropdown.Item>
+                            <NavDropdown.Item href="#/work">Work</NavDropdown.Item>
+                            <NavDropdown.Item href="#/faq">F&Q</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#/about">About</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
         </div>
     )
 }
-export default Nav;
+export default MyNav;
