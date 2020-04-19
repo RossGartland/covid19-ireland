a17061463e45b894ecd126b712ec7499a75291c8
Ross Gartland <rgartland117@hotmail.com>
2020-04-19 13:56:59 +0100

    Collapsible charts

--- src/components/charts/charts.component.jsx
+++ src/components/charts/charts.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Collapsible from 'react-collapsible';
 import ChartsNI from './ni-charts/charts-ni.component';
 import ChartsRoi from './roi-charts/charts-roi.component';
 import ChartsIsland from './island-charts/charts-island.component';
@@ -16,9 +17,17 @@ class Charts extends React.Component {
     render() {
         return (
             <React.Fragment>
+            <Collapsible trigger="View Northern Ireland Charts" triggerWhenOpen="Close Northern Ireland Charts">
             <ChartsNI />
+            </Collapsible>
+            <hr/>
+            <Collapsible trigger="View Republic of Ireland Charts" triggerWhenOpen="Close Republic of Ireland Charts">
             <ChartsRoi/>
+            </Collapsible>
+            <hr/>
+            <Collapsible trigger="View Island Wide Charts" triggerWhenOpen="Close Island Wide Charts">
             <ChartsIsland/>
+            </Collapsible>            
             </React.Fragment>
         )
     }
